3d2d8bbeddb0e703ee346613ed8368b5b109be5e
LEE HANYONG <glhy0718@gmail.com>
2019-08-17 01:52:21 +0900

    no message

--- TwoStarsFightGame/Assets/Scripts/Player.cs
+++ TwoStarsFightGame/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     public int health = 100;
     public Weapon currentWeapon = null;
+    public PlayerNumber playerNo;
 
     [SerializeField]
     private Weapon defaultWeapon;
@@ -20,6 +21,7 @@ public class Player : MonoBehaviour
         if (health < value)
         {
             health = 0;
+            GameManager.inst.PlayerDead(playerNo);
             return false;
         }
         else
