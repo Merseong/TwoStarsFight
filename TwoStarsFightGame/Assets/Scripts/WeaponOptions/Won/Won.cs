8efdf74d34c5d1313a75b1b9767328c3004a0b62
19 김동민 <dongmin11004@korea.ac.kr>
2019-08-16 22:14:24 +0900

    무기별 기본적인 스켈레톤

---
+++ TwoStarsFightGame/Assets/Scripts/WeaponOptions/Won/Won.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Won : Weapon, RangeWeapon, HandWeapon
+{
+    public void Action()
+    {
+
+    }
+
+    public override void AttackA()
+    {
+
+    }
+
+    public override void AttackB()
+    {
+
+    }
+
+    public override void Break()
+    {
+        equipPlayer.currentWeapon = this as Weapon;
+    }
+
+    public void Guard()
+    {
+
+    }
+
+    public void HitAction()
+    {
+ 
+    }
+
+    public override void ModeChange()
+    {
+        return;
+    }
+
+    public void Parrying()
+    {
+
+    }
+
+    public void Shoot(Vector2 start, Vector2 direction)
+    {
+
+    }
+}
