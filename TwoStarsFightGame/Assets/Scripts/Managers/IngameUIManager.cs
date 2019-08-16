c167057fffecbd3ff6aa5008d78d7fac731ec088
19 김동민 <dongmin11004@korea.ac.kr>
2019-08-17 01:40:47 +0900

    IngameUIManager 스켈레톤 / playtime UI / 플레이어의 hp바 UI 완성

---
+++ TwoStarsFightGame/Assets/Scripts/Managers/IngameUIManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IngameUIManager : SingletonBehaviour<IngameUIManager>
+{
+    public Text playtimeText;
+    public GameObject hpPrefab;
+
+    public GameObject[] Player1_hp = new GameObject[100];
+    public GameObject[] Player2_hp = new GameObject[100];
+    private void Start()
+    {
+        if (inst != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+            SetStatic();
+    }
+
+
+    public void UpdatePlaytimeText(float time)
+    {
+        string seconds = time.ToString("00");
+        playtimeText.text = seconds;
+    }
+
+    public void UpdatePlayerHP(int hp, bool playertype)
+    {
+        if (playertype)
+        {
+            for(int i = 0; i< 100; i++)
+            {
+                if( i < 100 - hp)
+                    Player1_hp[i].GetComponent<Image>().enabled = false;
+            }
+                
+        }
+        else
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i >=  hp)
+                    Player2_hp[i].GetComponent<Image>().enabled = false;
+            }
+
+        }
+    }
+}
