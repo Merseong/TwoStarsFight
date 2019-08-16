9759c8cbf8aea9f216e48481b243c76817a7de38
18신대성 <esc990720@korea.ac.kr>
2019-08-17 05:31:08 +0900

    트리거 이동해서 공격하게

--- TwoStarsFightGame/Assets/Scripts/PlayerController.cs
+++ TwoStarsFightGame/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,7 @@ public class PlayerController : MonoBehaviour
 {
     public int playerControl;
     public SkeletonAnimation skeleton;
+    public Animator anim;
     public Player player;
     public float delayTimer = 0f;
     public PlayerState playerState = PlayerState.Idle;
@@ -77,6 +78,7 @@ public class PlayerController : MonoBehaviour
         {
             if (Input.GetButtonDown("BasicAttack" + playerControl + "P"))
             {
+                anim.SetTrigger("ATTACK_BASIC_1");
                 skeleton.AnimationState.SetAnimation(1, "ATTACK_BASIC_1", false);
                 player.currentWeapon.AttackA();
             }
