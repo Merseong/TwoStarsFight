bede6467c961bebf316d6a38aa8b12e9675e8f7c
18신대성 <esc990720@korea.ac.kr>
2019-08-18 13:33:03 +0900

    아직 안됨 ㅅㄱ

--- TwoStarsFightGame/Assets/Scripts/Weapon.cs
+++ TwoStarsFightGame/Assets/Scripts/Weapon.cs
@@ -169,35 +169,6 @@ public abstract class Weapon : MonoBehaviour
     public abstract void DownAttackA();
     public abstract void DownAttackB();
 }
-
-[CreateAssetMenu]
-public class WeaponOption : ScriptableObject
-{
-    [Header("무기의 옵션들")]
-    public string weaponName;
-    public WeaponType weaponType;
-    [Tooltip("무기의 데미지")]
-    public int damage;
-    [Tooltip("공격시 감소하는 내구도")]
-    public int minusDurability;
-    [Tooltip("넉백 시간")]
-    public float knockbackTime;
-    [Tooltip("경직 시간")]
-    public float stiffTime;
-    [Tooltip("신기한 액션 여부")]
-    public bool haveAction;
-    [Header("공격자의 옵션들")]
-    [Tooltip("선딜")]
-    public float startTime;
-    [Tooltip("애니메이션 타임")]
-    public float _animTime;
-    public float animTime { get { return _animTime - (startTime + endTime); } }
-    [Tooltip("후딜")]
-    public float endTime;
-    [Tooltip("쿨타임")]
-    public float coolTime;
-}
-
 public interface RangeWeapon
 {
     void Shoot(Vector2 start, Vector2 direction);
---
+++ TwoStarsFightGame/Assets/Scripts/WeaponOption.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class WeaponOption : ScriptableObject
+{
+    [Header("무기의 옵션들")]
+    public string weaponName;
+    public WeaponType weaponType;
+    [Tooltip("무기의 데미지")]
+    public int damage;
+    [Tooltip("공격시 감소하는 내구도")]
+    public int minusDurability;
+    [Tooltip("넉백 시간")]
+    public float knockbackTime;
+    [Tooltip("경직 시간")]
+    public float stiffTime;
+    [Tooltip("신기한 액션 여부")]
+    public bool haveAction;
+    [Header("공격자의 옵션들")]
+    [Tooltip("선딜")]
+    public float startTime;
+    [Tooltip("애니메이션 타임")]
+    public float _animTime;
+    public float animTime { get { return _animTime - (startTime + endTime); } }
+    [Tooltip("후딜")]
+    public float endTime;
+    [Tooltip("쿨타임")]
+    public float coolTime;
+}
